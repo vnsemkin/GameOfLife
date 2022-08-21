926f0edb1f5b73f061079d1901f9234ef82408af
Vladimir Semkin <vnsemkin@gmail.com>
2022-08-21 13:59:29 +0300

    version 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File input = new File("input.txt");
+        File output = new File("output.txt");
+        String[] inputData;
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(input));
+        inputData = bufferedReader.readLine().split(",");
+        int axisX = Integer.parseInt(inputData[0]);
+        int axisY = Integer.parseInt(inputData[1]);
+        int cycleNumber = Integer.parseInt(inputData[2]);
+        String[][] oldGeneration = new String[axisX][axisY];
+        String[][] newGeneration = new String[axisX][axisY];
+        int count = 0;
+        while (bufferedReader.ready()) {
+            String[] rowData = bufferedReader.readLine().split(" ");
+            System.arraycopy(rowData, 0, oldGeneration[count], 0, axisX);
+            count++;
+        }
+        System.out.println("======Initial Generation======");
+        for (String[] string : oldGeneration) {
+            System.out.println(Arrays.toString(string));
+        }
+        int cycle = 0;
+        while (cycle < cycleNumber) {
+            cycle +=1;
+            for (int y = 0; y < axisY; y++) {
+                for (int x = 0; x < axisX; x++) {
+                    int neighbours = 0;
+                    int col;
+                    int row;
+                    if (oldGeneration[x][y].equals("X")) {
+                        neighbours -= 1;
+                    }
+                    for (int i = -1; i < 2; i++) {
+                        for (int j = -1; j < 2; j++) {
+                            col = (y + i + axisY) % axisY;
+                            row = (x + j + axisX) % axisX;
+                            if (oldGeneration[col][row].equals("X")) {
+                                neighbours++;
+                            }
+                        }
+                    }
+                    if ((oldGeneration[y][x].equals("X")) && (neighbours < 2)) {
+                        newGeneration[x][y] = "O";
+                    } else if ((oldGeneration[y][x].equals("X") && (neighbours > 3))) {
+                        newGeneration[x][y] = "O";
+                    } else if ((oldGeneration[y][x].equals("O") && (neighbours == 3))) {
+                        newGeneration[x][y] = "X";
+                    } else
+                        newGeneration[x][y] = oldGeneration[x][y];
+                }
+            }
+            System.out.println("========Generation "+cycle+" ========");
+            for (String[] string : newGeneration) {
+                System.out.println(Arrays.toString(string));
+            }
+            oldGeneration = newGeneration;
+        }
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(output))){
+            for(String[] string: newGeneration)
+            {
+                bufferedWriter.write(Arrays.toString(string) + "\n");
+            }
+        }catch (IOException e){
+            e.getStackTrace();
+        }
+    }
+}
